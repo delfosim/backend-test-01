07935c58de1a47d7c77068ce059104657ef2af51
Thiago Rodrigues de Souza <thiago.engh@gmail.com>
2019-10-14 22:23:51 -0300

    Finalizado endpoint analysis

--- teste-1/delfos_sim/api_source/views.py
+++ teste-1/delfos_sim/api_source/views.py
@@ -5,7 +5,6 @@ from .serializers.user_serializer import UserSerializer, UserSerializerInfo
 import json
 
 
-# Create your views here.
 @api_view(['GET'])
 def list_user(request):
     users = User.objects.all()
@@ -18,6 +17,12 @@ def list_user(request):
 
 @api_view(['GET'])
 def analysis(request):
+    json_result = {}
     json_data = open('./data_info/analysis.json')
     data1 = json.load(json_data)
-    return Response(data1)
+    for index, val in enumerate(data1):
+        position = index + 1
+        name_var = 'var_{}'.format(position)
+        json_result[position] = {'chart': {'name': name_var},
+                                 'data': [val], 'table': {'var': name_var, 'sample_time': val[0], 'value': val[1]}}
+    return Response(json_result)
